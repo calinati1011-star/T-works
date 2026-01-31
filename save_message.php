e634cc0c3c7d9c1a48729ebc8961831303be60be
calina.ti.1011-star <calina.ti.1011@irigemscuole.it>
2026-01-31 22:22:05 +0100

    fixx 2

--- save_message.php
+++ save_message.php
@@ -1,12 +1,12 @@
 <?php
-if ($_SERVER['REQUEST_METHOD']==='POST'){
+if($_SERVER['REQUEST_METHOD']==='POST'){
     $nome = htmlspecialchars(trim($_POST['nome']));
     $cognome = htmlspecialchars(trim($_POST['cognome']));
     $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
     $messaggio = htmlspecialchars(trim($_POST['messaggio']));
     if(!empty($nome) && !empty($cognome) && filter_var($email,FILTER_VALIDATE_EMAIL) && !empty($messaggio)){
-        $file='messaggi.txt';
-        $data="Nome: $nome\nCognome: $cognome\nEmail: $email\nMessaggio: $messaggio\n----------------------\n";
+        $file = 'messaggi.txt';
+        $data = "Nome: $nome\nCognome: $cognome\nEmail: $email\nMessaggio: $messaggio\n----------------------\n";
         file_put_contents($file,$data,FILE_APPEND|LOCK_EX);
         echo "ok";
     }else{
